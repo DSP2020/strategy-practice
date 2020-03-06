63c2b9a13aaaaaefb3f6f7b77d97f26632c7d629
RaulMendezA <rulax2313@gmail.com>
2020-03-05 23:55:16 -0600

    Checkpoints 1 2

---
+++ src/main/java/mx/iteso/mariokart/behaviors/impl/Fast.java
@@ -0,0 +1,9 @@
+package mx.iteso.mariokart.behaviors.impl; //ut
+import mx.iteso.mariokart.behaviors.ifAcceleration;
+
+public class Fast implements ifAcceleration {
+    @Override
+    public String setAcceleration() {
+        return "Going fast";
+    }
+}
---
+++ src/main/java/mx/iteso/mariokart/behaviors/impl/VeryFast.java
@@ -0,0 +1,9 @@
+package mx.iteso.mariokart.behaviors.impl; //ut
+import mx.iteso.mariokart.behaviors.ifAcceleration;
+
+public class VeryFast implements ifAcceleration {
+    @Override
+    public String setAcceleration() {
+        return "Going very fast";
+    }
+}
