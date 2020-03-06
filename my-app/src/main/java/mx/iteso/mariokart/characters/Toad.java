9a87dd0d71db820e70156a49f914da488095045f
user.email <is708346@iteso.mx>
2020-03-05 20:55:20 -0600

    Modificación de Toad

--- my-app/src/main/java/mx/iteso/mariokart/characters/Toad.java
+++ my-app/src/main/java/mx/iteso/mariokart/characters/Toad.java
@@ -2,9 +2,10 @@ package mx.iteso.mariokart.characters;
 
 import mx.iteso.mariokart.MarioKartCharacter;
 import mx.iteso.mariokart.behaviors.Acceleration;
+import mx.iteso.mariokart.behaviors.impl.Fast;
 import mx.iteso.mariokart.behaviors.impl.VeryFast;
 
 public class Toad extends MarioKartCharacter {
-	Acceleration toadAc = new VeryFast();
+	Acceleration toadAc = new Fast();
 	
 }
